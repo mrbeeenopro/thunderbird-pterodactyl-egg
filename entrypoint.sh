c3d6a9798a7c3e8ccc005cf1d61ec5d8efa27602
mrbeenopro <mrbeenopro@gmail.com>
2026-01-28 11:23:53 +0700

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd /home/container
+
+# Startup
+MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
+echo ":/home/container$ ${MODIFIED_STARTUP}"
+
+eval ${MODIFIED_STARTUP}
